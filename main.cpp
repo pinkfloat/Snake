8e3181a6bf285c4631fd9041a6b79c31624282ee
Sabrina Otto <otto.sabrina.berlin@googlemail.com>
2020-03-12 12:24:10 +0100

    Einige Leerzeilen aus main entfernt

--- main.cpp
+++ main.cpp
@@ -7,31 +7,26 @@ int main ( ) try {
 	Game game;
 
 	for (;;) {
-
 		Uint32 ticks_start = SDL_GetTicks();
 		
 		game.isRunning = game.player->getKeyInput();
-
 		if (!game.isRunning)
 			break;
 		
 		game.isRunning = game.update();
-	
 		if (!game.isRunning)
 			break;	
 			
 		game.window->draw();
 
 		Uint32 time = SDL_GetTicks() - ticks_start;
-
 		if (time < FRAMERATE)
 			SDL_Delay(FRAMERATE - time);	
 	}
-
 	printf("Your apples: %ld\n", game.player->Parts.size()-2);
-
 	return 0;
 }
+
 catch (std::runtime_error& e) {
 	fprintf(stderr, "Error: %s\n", e.what());
 	return 1;
